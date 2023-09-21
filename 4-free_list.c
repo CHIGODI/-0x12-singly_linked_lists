e3129d7de4feafa85e4d6a8da0b9d7fbc3d7ec07
CHIGODI <chiegody254@gmail.com>
2023-09-22 00:00:14 +0300

    Edit file 4-free_list

--- 4-free_list.c
+++ 4-free_list.c
@@ -9,5 +9,13 @@
  */
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *current = head;
+	list_t *next;
+
+	while(current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current  = next;
+	}
 }
